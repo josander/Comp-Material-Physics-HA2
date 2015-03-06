3b6f5fa8fb21ff5b2d7ec218054e3dbbdca33241
Josephine <jossan_cuellar@hotmail.com>
2015-03-06 19:40:21 +0100

    Can now plot the potential

--- HA2.m
+++ HA2.m
@@ -1,4 +1,6 @@
 %% Home assignment 2
+% Task 1: Plot the periodic potential in the plane (-1 1 0)
+
 clc
 clf
 clear all
@@ -33,30 +35,45 @@ v_G = [0 -0.056 0.0138 0.0181];
 % Get potential in reciprocal space
 V_G = v_G.*S_G;
 
+% Number of grid points
+nPoints = 100;
+
 % Get vector for the 3D space
 for i = 1:3
-    r(i,:) = linspace(1,10,100);
+    r(i,:) = linspace(1,10,nPoints);
 end
 
-% Calc potential in space. j: distance from origo in one direction. i:sum
-% over different G-vectors
-for j = 1:length(r)
-    for i = 1:Gsize(1)
-        V_r(i,j) =  V_G(1,i) * cos(norm(G(i,:).*r(:,j)'));
+% Initialise with zeros
+V_r = zeros(nPoints, nPoints, nPoints);
+
+% Calc potential in space
+for k = 1:length(r)
+    for l = 1:length(r)
+        for m = 1:length(r)
+            for i = 1:Gsize(1)
+                V_r(k,l,m) =  V_r(k,l,m) + V_G(1,i) * cos(norm(G(i,:).*[r(1,k) r(2,l) r(3,m)]));
+            end
+        end
     end
 end
 
-%%
+% Get the potential in the plane
 for i = 1:length(r)-1
-    plot3(V_r(1,i), V_r(2,i+1), V_r(3,:))
-    hold on
+    potInPlan(i,:) = V_r(i,i+1,:); 
 end
 
-hold off
+% Plot the potential in the plane
+surf(potInPlan)
 
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
 
+shg
+
+%% Task 2: Get band structure
 
 
+k(1,:) = [0 0 0];
+k(2,:) = pi/a*[-1 1 1];
+k(3,:) = 2*pi/a*[0 0 1];
